38b973d0d5b0db525a0dd99cb75cacd2689295cb
ha0372032020 <ha0372032020@unab.edu.sv>
2021-05-09 21:42:21 -0600

    ejerGII

---
+++ EjercicioG2/EjercicioG2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Ingrese su nombre: ");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Ingrese su sexo (Mujer = M y Hombre = H)");
+            Char genero = Convert.ToChar(Console.ReadLine());
+
+            switch (genero)
+            {
+                case 'M':
+                    Console.WriteLine("La Sra.{0}", nombre);
+                    break;
+
+                case 'H':
+                    Console.WriteLine("El Sr.{0}", nombre);
+                    break;
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
